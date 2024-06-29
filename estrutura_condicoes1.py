6a351fdea5407ed4775cba72b956d9c9adc13b41
Liliane Moreira <106605093+Alanins@users.noreply.github.com>
2024-06-29 07:00:37 -0300

    Add files via upload

---
+++ estrutura_condicoes1.py
@@ -0,0 +1,11 @@
+#Microatividade1 - Utilização das estruturas de condição if e else em Python
+
+temperatura=31
+
+if(temperatura<30):
+    resultado=("A temperatura hoje está amena.")
+
+else:
+    resultado=("Hoje está fazendo calor.")
+
+print(resultado)
