a56f85ff7327b8f8e948d4df20b7f6b9c4af14ff
Liliane Moreira <106605093+Alanins@users.noreply.github.com>
2024-06-29 07:02:18 -0300

    Add files via upload

---
+++ estrutura_repeticao2.py
@@ -0,0 +1,16 @@
+#Microatividade 4: Descrever a utilização da estrutura de repetição for em Python
+
+texto="Olá, laço for."
+
+# Primeira instrução for
+for texto in "Olá, laço for.":
+     print(f"Caractere: {texto}")
+
+# Segunda instrução for
+for i in range(1, 11):
+  print(f"Número do intervalo: {str(i)}")
+
+
+
+
+    
